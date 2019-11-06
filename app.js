a14b2085116c1ec8775fe40a322e7b4d89fdad94
Jon Maldia <jon@maldia.net>
2019-11-06 14:50:13 -0500

    Add routes

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 let express= require("express");
 let app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hi there bro!");
+});
+
+app.get("/bye", (req, res) => {
+    res.send("Bye bro!");
+});
+
+app.get("/dog", (req, res) => {
+    res.send("Bow wow!");
+});
+
+
+// Tell express to listen forrequests -start server 
+// start your app with this command: PORT=3000 node app.js
+app.listen(process.env.PORT, process.env.IP, () =>{
+    console.log("Server has started")
+});
